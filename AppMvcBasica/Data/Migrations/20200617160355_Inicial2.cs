a1bc0e323e2655aae01ec110860b9f69118b8ae1
Icaro Henrique <icaro_henrick@hotmail.com>
2020-06-30 11:28:41 -0300

    Adicionando Projeto MVC Básico em dotNetCore 3.1 para fixação para o Git

---
+++ AppMvcBasica/Data/Migrations/20200617160355_Inicial2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppMvcBasica.Data.Migrations
+{
+    public partial class Inicial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Fornecedores_FornecedorId",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "ForncedorId",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FornecedorId",
+                table: "Produtos",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Fornecedores_FornecedorId",
+                table: "Produtos",
+                column: "FornecedorId",
+                principalTable: "Fornecedores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Fornecedores_FornecedorId",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FornecedorId",
+                table: "Produtos",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ForncedorId",
+                table: "Produtos",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Fornecedores_FornecedorId",
+                table: "Produtos",
+                column: "FornecedorId",
+                principalTable: "Fornecedores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
